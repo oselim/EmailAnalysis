331df5414e6c74e0dec9ab3a1b7fe3091c45ffb7
oselim <selimzcn@gmail.com>
2019-11-12 00:29:29 -0600

    email parsing implemented. vertexArray and EdgeArray are built.

--- src/main/scala/EmailParser.scala
+++ src/main/scala/EmailParser.scala
@@ -1,9 +1,10 @@
 import java.io.{ByteArrayInputStream, File}
 import java.util.Properties
-import javax.mail.{Address, Message, Session}
+import javax.mail.{Address, Session}
 import edu.phd.EmailParser.util.FileUtil
 import scala.io.Source
-import javax.mail.internet.MimeMessage
+import javax.mail.internet.{InternetAddress, MimeMessage}
+
 import scala.collection.mutable.ArrayBuffer
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
@@ -12,57 +13,76 @@ import org.apache.spark.graphx._
 
 
 object EmailParser extends App {
-  val session = Session.getDefaultInstance(new Properties() { put("mail.smtp.host", "host") })
 
-  private var fileUtil = new  FileUtil()
+  System.setProperty("mail.mime.address.strict", "false")
+
+  val session = Session.getDefaultInstance(new Properties() {
+    put("mail.smtp.host", "host")
+    put("mail.mime.address.strict", "false")
+  })
+
+  private var fileUtil = new FileUtil()
 
   private var files: Array[File] = fileUtil.recursiveListFiles(new File("enron-sample-dataset"))
 
   private var vertexArray = new ArrayBuffer[(Long, (String, String))]()
   private var edgeArray = new ArrayBuffer[(Long, Long, (String, String))]()
 
-  def parseAddresses(fromAddresses: Array[Address], toAddresses: Array[Address], ccAddresses: Array[Address], bccAddresses: Array[Address], xOriginHeader: Array[String], messageID: String): Unit = {
-    fromAddresses.par.foreach(from => {
+  def parseAddresses(fromAddresses: Array[Address], receiverAddresses: Array[Address], xOriginHeader: Array[String], messageID: String): Unit = {
+    fromAddresses.foreach(from => {
       //TODO algoritma yazilmali
       //TODO from base alinarak her bir from icin bir vertex eklenip butun to cc bcc dolasilip vertexler eklenip, egdeler eklenmeli. Her bir vertex icin contains controlu yapilmali. edgeler icin de contains kontrolu yapilabilir.
 
+
+      val xOrigin = xOriginHeader(0)
+      val fromString = from.toString
+      addToVertex(fromString, xOrigin, messageID)
+
+      receiverAddresses.foreach(receiver => {
+        val receiverString = receiver.toString
+        addToVertex(receiverString, xOrigin, messageID)
+        addToEdge(fromString, receiverString, xOrigin, messageID)
+      })
     })
   }
 
-  def addToVertex( email: String, xOrigin: String, messageId:String ): Unit ={
-    //TODO vertexID olusturulacak, hash ve ya getBytes ile
-    //TODO contains ile kontrol edilecek
+  def addToVertex(email: String, xOrigin: String, messageId: String): Unit = {
+    var vID = 0L
+    if (!email.equals("default"))
+      vID = email.hashCode.toLong
+
+    if (!vertexArray.exists(vertex => vertex._1.equals(vID))) {
+      vertexArray.append((vID, (xOrigin, messageId)))
+    }
   }
 
-  def addToEdge( sourceAddress: String, destinationAddress:String, xOrigin: String, messageId:String): Unit ={
-    //TODO sourceAddress ve destinationAddress long tipine donusturulecek, hash ve ya getbytes ile
-    //TODO contains ile kontrol edilecek
-    //TODO https://www.youtube.com/watch?v=QV6L1zRQ6JM&list=PLkK1Z8GOwcNNIhfdiioIoLVTHVj49GqmS&index=7
-    
+  def addToEdge(sourceAddress: String, destinationAddress: String, xOrigin: String, messageId: String): Unit = {
+    val sourceID = sourceAddress.hashCode.toLong
+    var destinationID = 0L
+    if (!destinationAddress.equals("default"))
+      destinationID = destinationAddress.hashCode.toLong
+    edgeArray.append((sourceID, destinationID, (xOrigin, messageId)))
   }
 
-  files.par.foreach(file => {
+  files.foreach(file => {
     val source = Source.fromFile(file)
     val fileString: String = source.mkString
     val mimeMessage = new MimeMessage(session, new ByteArrayInputStream(fileString.getBytes))
     val fromAddresses = mimeMessage.getFrom
-    val toAddresses = mimeMessage.getRecipients( Message.RecipientType.TO)
-    val ccAddresses = mimeMessage.getRecipients( Message.RecipientType.CC)
-    val bccAddresses = mimeMessage.getRecipients( Message.RecipientType.BCC)
+    var allRecipients = mimeMessage.getAllRecipients
+    if (allRecipients == null) {
+      allRecipients = Array.apply(new InternetAddress("default", false))
+    }
     val xOriginHeader = mimeMessage.getHeader("X-Origin")
     val messageID = mimeMessage.getMessageID
 
-    parseAddresses(fromAddresses, toAddresses, ccAddresses, bccAddresses, xOriginHeader, messageID)
+    parseAddresses(fromAddresses, allRecipients, xOriginHeader, messageID)
 
     source.close()
   })
 
   val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
   val sc = new SparkContext(conf)
-
-
-
-
 
 
 }
