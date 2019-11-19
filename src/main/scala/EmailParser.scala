8bb99cc36730b75ea300d913d5ec3d7a7a0323bd
Selim Ozcan <selimzcn@gmail.com>
2019-11-18 18:20:59 -0600

    pagerank, trianglecount added, output structured

--- src/main/scala/EmailParser.scala
+++ src/main/scala/EmailParser.scala
@@ -12,6 +12,7 @@ import scala.collection.mutable.ArrayBuffer
 import org.apache.spark._
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.{ArrayType, LongType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 // import classes required for using GraphX
 import org.apache.spark.graphx._
@@ -30,43 +31,41 @@ object EmailParser extends App {
 
   private var files: Array[File] = fileUtil.recursiveListFiles(new File("enron-sample-dataset"))
 
-  private var vertexArray = new ArrayBuffer[(Long, (String, String, String))]()
-  private var edgeArray = new ArrayBuffer[Edge[ (String, String) ]]()
+  private var vertexArray = new ArrayBuffer[(Long, String)]()
+  private var edgeArray = new ArrayBuffer[Edge[(String, String)]]()
 
   def parseAddresses(fromAddresses: Array[Address], receiverAddresses: Array[Address], xOriginHeader: Array[String], messageID: String): Unit = {
     fromAddresses.foreach(from => {
       //TODO algoritma yazilmali
       //TODO from base alinarak her bir from icin bir vertex eklenip butun to cc bcc dolasilip vertexler eklenip, egdeler eklenmeli. Her bir vertex icin contains controlu yapilmali. edgeler icin de contains kontrolu yapilabilir.
 
-
-      val xOrigin = xOriginHeader(0)
       val fromString = from.toString
-      addToVertex(fromString, xOrigin, messageID)
+      addToVertex(fromString)
 
       receiverAddresses.foreach(receiver => {
         val receiverString = receiver.toString
-        addToVertex(receiverString, xOrigin, messageID)
-        addToEdge(fromString, receiverString, xOrigin, messageID)
+        addToVertex(receiverString)
+        addToEdge(fromString, receiverString)
       })
     })
   }
 
-  def addToVertex(email: String, xOrigin: String, messageId: String): Unit = {
+  def addToVertex(email: String): Unit = {
     var vID = 0L
     if (!email.equals("default"))
       vID = email.hashCode.toLong
 
     if (!vertexArray.exists(vertex => vertex._1.equals(vID))) {
-      vertexArray.append((vID, (xOrigin, email, messageId)))
+      vertexArray.append((vID, email))
     }
   }
 
-  def addToEdge(sourceAddress: String, destinationAddress: String, xOrigin: String, messageId: String): Unit = {
+  def addToEdge(sourceAddress: String, destinationAddress: String): Unit = {
     val sourceID = sourceAddress.hashCode.toLong
     var destinationID = 0L
     if (!destinationAddress.equals("default"))
       destinationID = destinationAddress.hashCode.toLong
-    edgeArray.append(Edge(sourceID, destinationID, (xOrigin, messageId)))
+    edgeArray.append(Edge(sourceID, destinationID, (sourceAddress, destinationAddress)))
   }
 
   files.foreach(file => {
@@ -76,7 +75,7 @@ object EmailParser extends App {
     val fromAddresses = mimeMessage.getFrom
     var allRecipients = mimeMessage.getAllRecipients
     if (allRecipients == null) {
-      allRecipients = Array.apply(new InternetAddress("default", false))
+      allRecipients = Array.apply(new InternetAddress("NonRecipient", false))
     }
     val xOriginHeader = mimeMessage.getHeader("X-Origin")
     val messageID = mimeMessage.getMessageID
@@ -93,34 +92,34 @@ object EmailParser extends App {
   val sparkSession = SparkSession.builder.config(sc.getConf).getOrCreate()
 
 
-  private val vertexRDD: RDD[(VertexId, (String, String, String))] = sc.parallelize(vertexArray)
+  private val vertexRDD: RDD[(VertexId, String)] = sc.parallelize(vertexArray)
   private val edgeRDD: RDD[Edge[(String, String)]] = sc.parallelize(edgeArray)
 
   private val graph = Graph(vertexRDD, edgeRDD)
   println(graph.numEdges)
   println(graph.numVertices)
 
-  case class OutputClass(id: Long, pageRank: Double, email: String)
 
   private val pageRank: Graph[Double, Double] = graph.pageRank(0.0001)
-  private val sortedPageRankGraph: RDD[(VertexId, (Double, (String, String, String)))] = pageRank.vertices.join(vertexRDD).sortBy(_._2._1, ascending = false)
-//  println(sortedPageRankGraph.collect().deep)
+
+  private val triangleCounts: Graph[PartitionID, (String, String)] = graph.triangleCount()
 
 
-  //  println(graph.inDegrees.collect().deep )
-  //  println(graph.outDegrees.collect().deep )
+  case class OutputClass(id: Long, pageRank: Option[Double], email: Option[String], inDegree: Option[Int], outDegree: Option[Int], triangleCount: Option[Int])
+  private val vertexDF: DataFrame = sparkSession.createDataFrame(
 
-
-  private val vertexDF: DataFrame = sparkSession.createDataFrame(sortedPageRankGraph.map(x => OutputClass(x._1, x._2._1, x._2._2._2) ) )
-
-  vertexDF.show(truncate = false)
-
-/*
-  graph.vertices.filter(vertex => {
+    graph.vertices.fullOuterJoin(pageRank.vertices).fullOuterJoin(graph.inDegrees).fullOuterJoin(graph.outDegrees).fullOuterJoin(triangleCounts.vertices)
+      .map(vertex => {
     val vertexID = vertex._1
-    graph.
-  })
-*/
+    val pageRankValue = vertex._2._1.get._1.get._1.get._2
+    val email = vertex._2._1.get._1.get._1.get._1
+    val inDegree = vertex._2._1.get._1.get._2
+    val outDegree = vertex._2._1.get._2
+    val triangleCount = vertex._2._2
 
+    OutputClass(vertexID, pageRankValue, email, inDegree, outDegree, triangleCount)
+  } ) )
+
+  vertexDF.show(571, truncate = false)
 
 }
